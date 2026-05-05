03072e2d6619810fb97538fa0bc1d4260308fd57
alexgamerh3k4-lab <alexgamerh3k4@gmail.com>
2026-05-05 14:14:01 +0200

    TILDES NOP

--- Assets/Scripts/Letras.cs
+++ Assets/Scripts/Letras.cs
@@ -21,7 +21,7 @@ public class Letras : MonoBehaviour
     {
 
         // Guardamos los textos en variables
-        string t1 = "Día 1";
+        string t1 = "Dia 1";
         string t2 = "Humanos : " + Humanos;
         string t3 = "Monstruos : " + Monstruos;
         string t4 = "";
@@ -29,27 +29,27 @@ public class Letras : MonoBehaviour
         // TU LÓGICA (sin cambiarla mucho)
         if (Humanos == 0 && Monstruos == 0)
         {
-            t4 = "Durante el día no se presencio nada de vida, solo llegaste a la vida y solo te irás de él,... Los soldados te dan el periodico y ves como los accidentes e incidentes bajan la poblacion del país";
+            t4 = "Durante el dia no se presencio nada de vida, solo llegaste a la vida y solo te iras de el,... Los soldados te dan el periodico y ves como los accidentes e incidentes bajan la poblacion del pais";
         }
         else if (Humanos > 0 && Humanos < 3 && Monstruos == 0)
         {
-            t4 = "Últimamente no se registró ningun monstruo... Sin embargo, te sientes como un monstruo por no haber dejado pasar a los que faltaba, en la radio escuchas algunos informes sobre quejas de personas que no pudieron pasar la frontera. Los soldados estan mas pendientes";
+            t4 = "Ultimamente no se registro ningun monstruo... Sin embargo, te sientes como un monstruo por no haber dejado pasar a los que faltaba, en la radio escuchas algunos informes sobre quejas de personas que no pudieron pasar la frontera. Los soldados estan mas pendientes";
         }
         else if (Humanos == 3 && Monstruos == 0)
         {
-            t4 = "Entre todas las personas que pasaron, reconoces al hermano de un amigo tuyo, te alegras bastante de que este vivo y haya podido pasar la frontera. Los soldados bajan la guardia y empiezan a hablar entre ellos, desde la ventana escuchas que hablan sobre algunos datos que esconde el gobierno sobre los informes de los dentistas de las personas ¿Qué tendra que ver eso con los rumores de los monstruos";
+            t4 = "Entre todas las personas que pasaron, reconoces al hermano de un amigo tuyo, te alegras bastante de que este vivo y haya podido pasar la frontera. Los soldados bajan la guardia y empiezan a hablar entre ellos, desde la ventana escuchas que hablan sobre algunos datos que esconde el gobierno sobre los informes de los dentistas de las personas ¿Que tendra que ver eso con los rumores de los monstruos?";
         }
         else if (Humanos == 0 && Monstruos > 0)
         {
-            t4 = "Durante el día no se ha registrado ningun monstruo, Feliz día del reves. Uno de los soldados desapareció de la guardia y el otro dijo que lo último que vio fue su zapado ensangrentado. ¡¿Qué le esta pasando al mundo?!";
+            t4 = "Durante el dia no se ha registrado ningun monstruo, Feliz dia del reves. Uno de los soldados desaparecio de la guardia y el otro dijo que lo ultimo que vio fue su zapado ensangrentado. ¡¿Que le esta pasando al mundo?!";
         }
         else if (Humanos > 0 && Humanos < 3 && Monstruos > 0)
         {
-            t4 = "Durante el día se ha notificado personas con aspecto monstruoso atacando gente y soldados,entre ellos, uno de tus soldados quedó abatido, por lo que cuentas solo con uno para la guardia, no sabes porque pero, sientes que parte de esto es por tu culpa";
+            t4 = "Durante el dia se ha notificado personas con aspecto monstruoso atacando gente y soldados,entre ellos, uno de tus soldados quedo abatido, por lo que cuentas solo con uno para la guardia, no sabes porque pero, sientes que parte de esto es por tu culpa";
         }
         else if (Humanos == 3 && Monstruos > 0)
         {
-            t4 = "En la radio se habla de una asamblea de muchas personas atacando a varios barrios, se empieza a mencionar que, por sus rasgos, parecen que no son humanos de verdad y empiezan a denominarlos como monstruos. Después de escuchar la radio, decides asomar la cabeza y ves como uno de los soldados se lo lleva el equipo medico por varios ataques recibidos. El otro sigue con su trabajo, pero se le ve asustado";
+            t4 = "En la radio se habla de una asamblea de muchas personas atacando a varios barrios, se empieza a mencionar que, por sus rasgos, parecen que no son humanos de verdad y empiezan a denominarlos como monstruos. Despues de escuchar la radio, decides asomar la cabeza y ves como uno de los soldados se lo lleva el equipo medico por varios ataques recibidos. El otro sigue con su trabajo, pero se le ve asustado";
    
         }
 
